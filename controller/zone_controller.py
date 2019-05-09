adf6bcb89fe56f397e00775997b0e8c803bdfb73
Anis Boubaker <46570545+AnisBoubaker@users.noreply.github.com>
2019-05-09 15:28:01 -0400

    Update zone_controller.py
    Added comments to controller script.

--- controller/zone_controller.py
+++ controller/zone_controller.py
@@ -18,8 +18,8 @@ for key, pin in pinList.items():
     GPIO.output(pin, GPIO.LOW)
 
 
+# Parse command line arguments
 argv = sys.argv[1:]
-
 try:
     opts, args = getopt.getopt(argv,"hz:s:",["zone=","state="])
 except getopt.GetoptError:
@@ -38,19 +38,18 @@ for opt, arg in opts:
     elif opt in ("-s", "--state"):
         state=arg
 
+# Error contol: State could be either ON or OFF
 if state not in ('ON', 'OFF'):
     print 'zones.py -z <number> -s <ON|OFF>'
     sys.exit(3)
 
-print('la zone: ',zone)
-
+# Error contol: Zone must be between 1..4
 if zone<1 or zone>4:
     print 'Invalid zone (1..4)'
     sys.exit(4)
 
 
-
-# active each relay for 2 seconds with 2 second delay between each activation
+# Activate/Deactivate the selected zone
 if state=='ON':
     GPIO.output(pinList[zone], GPIO.HIGH)
 else:

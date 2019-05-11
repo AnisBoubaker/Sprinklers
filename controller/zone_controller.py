706b07601a4de4e50a05fc0c1c92c0a4bd6fbc10
Anis Boubaker <anis@boubaker.ca>
2019-05-10 20:45:21 -0400

    Controller: refactoring and added the ability to check the status of a given zone

--- controller/zone_controller.py
+++ controller/zone_controller.py
@@ -12,49 +12,70 @@ GPIO.setwarnings(False)
 
 pinList = {1:4, 2:22, 3:6, 4:26}
 
+
+def set_zone(zone_num, state):
+    for key, pin in pinList.items():
+        # loop through pins and set mode default state is 'low'
+        GPIO.output(pin, GPIO.LOW)
+
+    # Activate/Deactivate the selected zone
+    if state=='ON':
+        GPIO.output(pinList[zone], GPIO.HIGH)
+    else:
+        GPIO.output(pinList[zone], GPIO.LOW)
+
+def get_zone(zone_num):
+    status = GPIO.input(pinList[zone])
+    if status:
+        return 'On'
+    else:
+        return 'Off'
+
+
 # Setup pins and shuts down all the zones
 for key, pin in pinList.items():
     # loop through pins and set mode default state is 'low'
     GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
 
-
-# Parse command line arguments
-argv = sys.argv[1:]
-try:
-    opts, args = getopt.getopt(argv,"hz:s:",["zone=","state="])
-except getopt.GetoptError:
-    print 'zones.py -z <number> -s <ON|OFF>'
-    sys.exit(2)
-
-state = ''
-zone = 0
-
-for opt, arg in opts:
-    if opt == '-h':
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"hz:s:",["zone=","state="])
+    except getopt.GetoptError:
         print 'zones.py -z <number> -s <ON|OFF>'
-        sys.exit()
-    elif opt in ("-z", "--zone"):
-        zone=int(arg)
-    elif opt in ("-s", "--state"):
-        state=arg
+        sys.exit(2)
 
-# Error contol: State could be either ON or OFF
-if state not in ('ON', 'OFF'):
-    print 'zones.py -z <number> -s <ON|OFF>'
-    sys.exit(3)
+    state = ''
+    zone = 0
 
-# Error contol: Zone must be between 1..4
-if zone<1 or zone>4:
-    print 'Invalid zone (1..4)'
-    sys.exit(4)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'zones.py -z <number> -s <ON|OFF>'
+            sys.exit()
+        elif opt in ("-z", "--zone"):
+            zone=int(arg)
+        elif opt in ("-s", "--state"):
+            state=arg
+
+    # Error contol: State could be either ON or OFF
+    if state not in ('ON', 'OFF', '?'):
+        print 'zones.py -z <number> -s <ON|OFF>'
+        sys.exit(3)
+
+    # Error contol: Zone must be between 1..4
+    if zone<1 or zone>4:
+        print 'Invalid zone (1..4)'
+        sys.exit(4)
 
 
-# Activate/Deactivate the selected zone
-if state=='ON':
-    GPIO.output(pinList[zone], GPIO.HIGH)
-else:
-    GPIO.output(pinList[zone], GPIO.LOW)
+    if state in ('ON', 'OFF'):
+        set_zone(zone, state)
+        print 'OK'
+    elif state == '?':
+        print(get_zone(zone))
 
-# Reset GPIO settings
-#GPIO.cleanup()
+    
+
+# If script is called directly
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    main(argv)
